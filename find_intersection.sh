023a176efeae15e0a91f549e76126e0710dcff73
Keesha Jazzlyn Cruz Go <113406963+kjcgo@users.noreply.github.com>
2026-02-18 13:11:30 -0500

    Update find_intersection.sh

--- find_intersection.sh
+++ find_intersection.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 bedtools intersect -a snps_full.bed -b Neuron1.bed Neuron2.bed NSC1.bed NSC2.bed iPSC1.bed iPSC2.bed > allregions.bed
-bedtools intersect -a snps_full.bed -b  NSC1.bed NSC2.bed > nsc_only.bed
+bedtools intersect -a snps_full.bed -b Neuron2.bed Neuron2.bed iPSC1.bed iPSC2.bed > neuron_iPSC_snps.bed
+bedtools intersect -v -a neuron_iPSC_snps.bed -b allregions.bed > nsc_exclusive.bed
